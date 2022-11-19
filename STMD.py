5787554a2881fa969d3213e9ed90e3c4d3c31165
Anindya Ghosh <ag803@sussex.ac.uk>
2022-11-19 00:50:00 +0000

    X-Y map of object moving

---
+++ STMD.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# X-Y map of object moving
+t_sim = 100
+vf = np.zeros((100, 100))
+
+height = 5
+ylims = (vf.shape[0]-height) // 2 + np.arange(height)
+start = [ylims, 0]
+
+def translation(t, v, x_init):
+    return v * t + x_init
+
+x = []
+for t in range(t_sim):
+    dist = translation(t, 5, x_init=start[1])
+    if dist < vf.shape[1]:
+        x.append(dist)
+        
+        vf[start[0], dist] = 1
+        
+# Neuron models - LIF
