ea16970c51a10e9edf5d367e2106e442f45ec03e
Anindya Ghosh <ag803@sussex.ac.uk>
2023-02-06 19:42:43 +0000

    Completed EMD with working implementation

--- EMD.py
+++ EMD.py
@@ -1,6 +1,9 @@
 """
 Adapted from Bagheri, Z.M., Wiederman, S.D., Cazzolato, B.S., Grainger, S. and O’Carroll, D.C., 2017. 
 Performance of an insect-inspired target tracker in natural conditions. Bioinspiration & biomimetics, 12(2), p.025006.
+
+Eichner, H., Joesch, M., Schnell, B., Reiff, D.F. and Borst, A., 2011. 
+Internal structure of the fly elementary motion detector. Neuron, 70(6), pp.1155-1164.
 """
 
 import matplotlib.pyplot as plt
@@ -35,7 +38,7 @@ def save_fig(title):
     plt.close()
 
 class EMD:
-    def __init__(self):
+    def __init__(self, **kwargs):
         self.HEIGHT = 16
         self.WIDTH = 16
         self.TIMESTEPS = 16
@@ -45,6 +48,8 @@ class EMD:
         self.STIM_WIDTH = 2
         self.STIM_Y = 2
         
+        self.direction = kwargs['DIRECTION']
+        
         # Time constants
         self.HPF_TAU = 40
         self.LPF_TAU = 35
@@ -53,22 +58,20 @@ class EMD:
         self.HPF_K = np.exp(-1 / self.HPF_TAU)
         self.LPF = np.exp(-1 / self.LPF_TAU)
         
-    def image_generation(self, **kwargs):
+    def image_generation(self):
         # Initialise images tensor
-        self.direction = kwargs['DIRECTION']
-        
         images = np.ones((self.TIMESTEPS, self.HEIGHT, self.WIDTH))
         for i in range(self.TIMESTEPS):
             start_pos_x = i*self.VELOCITY
             images[i,self.STIM_Y:self.STIM_Y+self.STIM_HEIGHT,start_pos_x:start_pos_x+self.STIM_WIDTH] = 0.0
-        if self.direction == 'right':
+        if self.direction == 'RIGHT':
             pass
         else:
             images = images[::-1,:,:]
         return images
         
     def model(self):
-        images = self.image_generation(DIRECTION='left')
+        images = self.image_generation()
         # Initialisations
 
         hpf = np.zeros((self.TIMESTEPS, self.HEIGHT, self.WIDTH))
@@ -79,10 +82,7 @@ class EMD:
         a_on = np.zeros((self.TIMESTEPS, self.HEIGHT, self.WIDTH))
         a_off = np.zeros((self.TIMESTEPS, self.HEIGHT, self.WIDTH))
         
-        on_out = np.zeros((self.TIMESTEPS, self.HEIGHT, self.WIDTH))
-        off_out = np.zeros((self.TIMESTEPS, self.HEIGHT, self.WIDTH))
-        
-        output = np.zeros((self.TIMESTEPS, self.HEIGHT, self.WIDTH))
+        output = np.zeros((self.TIMESTEPS, self.HEIGHT, self.WIDTH-1))
 
         for t in range(1, self.TIMESTEPS):
             # High-pass filters
@@ -97,23 +97,18 @@ class EMD:
             a_off[t] = ((1.0 - self.LPF) * off_f[t]) + (self.LPF * a_off[t - 1]) 
             
             # Correlate
-            on_out[t] = on_f[t] * a_on[t]
-            off_out[t] = off_f[t] * a_off[t]
-            
-            # Compare
-            on_comp = on_out[:-1,:,:] - on_out[1:,:,:]
-            off_comp = off_out[:-1,:,:] - off_out[1:,:,:]
+            on_out = (a_on[t,:,:-1] * on_f[t,:,1:]) - (on_f[t,:,:-1] * a_on[t,:,1:])
+            off_out = (a_off[t,:,:-1] * off_f[t,:,1:]) - (off_f[t,:,:-1] * a_off[t,:,1:])
             
             # Summation
-            output[t] = np.maximum(on_comp[t - 1] + off_comp[t - 1], 0.0)
+            output[t] = on_out + off_out
         
-        self.outs = {'hpf':hpf, 'on_f':on_f, 'off_f':off_f, 'a_on':a_on, 'a_off':a_off, 
-                'on_comp':on_comp, 'off_comp':off_comp, 'output':output}    
+        self.outs = {'hpf':hpf, 'on_f':on_f, 'off_f':off_f, 'a_on':a_on, 'a_off':a_off, 'output':output}    
         
         return self.outs
 
     def visualisations(self):
-        images = self.image_generation(DIRECTION='left')
+        images = self.image_generation()
         
         # Visualize stimuli
         fig, axes = plt.subplots(1,self.TIMESTEPS, sharey=True, figsize=(15,5))
@@ -125,17 +120,8 @@ class EMD:
         
         visualise([self.outs['on_f'], self.outs['off_f']], ['On', 'Off'], title='Rectified channels')
         visualise([self.outs['a_on'], self.outs['a_off']], ['On', 'Off'], title='Low-pass filtered channels')
-        visualise([self.outs['on_out'], self.outs['off_out']], ['On', 'Off'], title='Correlation')
-        visualise([self.outs['on_comp'], self.outs['off_comp']], ['On', 'Off'], title='Compare')
-        visualise([self.outs['output']], title='Output')
+        visualise([self.outs['output']], title=f'Output_{self.direction}')
         
-        # Visualise output
-        fig, axes = plt.subplots(1, self.TIMESTEPS, sharey=True, figsize=(15, 5))
-        for t, a in enumerate(axes):
-            a.plot(np.sum(self.outs['output'][t,:,:])), 
-                      vmin=0.0, 
-                      vmax=1.0, 
-                      cmap="gray")
-            hide_axes(a)
-        fig.tight_layout(pad=0)
-        save_fig('EMD output.png')
+EMD_model = EMD(DIRECTION='RIGHT')
+outs = EMD_model.model()
+EMD_model.visualisations()
